f8b2f8ce36690262583040f27c8d2e3add0b4f77
Duc Tri <pcldt123@gmail.com>
2024-08-14 19:10:12 +0700

    commit lai them function our_rooms lan thu 2

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -31,7 +31,42 @@
 @include('home.about')
 <!-- end about -->
 <!-- our_room -->
-@include('home.rooms')
+<div  class="our_room">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10">
+                <div class="titlepage">
+                    <h2>Our Room</h2>
+                    <p>Lorem Ipsum available, but the majority have suffered </p>
+                </div>
+            </div>
+        </div>
+
+        @if($room->isEmpty())
+            <div class="alert alert-warning">
+                No room available for the selected criteria
+            </div>
+        @else
+
+            <div class="row">
+                @foreach($room as $rooms)
+                    <div class="col-md-4 col-sm-6">
+                        <div id="serv_hover"  class="room">
+                            <div class="room_img">
+                                <figure><img style="height: 200px;width: 320px" src="room/{{$rooms->image}}" alt="#"/></figure>
+                            </div>
+                            <div class="bed_room">
+                                <h3>{{$rooms->room_title}}</h3>
+                                <p style="padding:10px ">{!! Str::limit($rooms->description,100) !!} </p>
+                                <a class="btn btn-primary" href="{{url('room_details',$rooms->id)}}">Room Details</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+                @endif
+            </div>
+    </div>
+</div>
 <!-- end our_room -->
 <!-- gallery -->
 @include('home.galary')
